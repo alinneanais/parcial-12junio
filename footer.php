05437fae66050533a2d967b45b9b58249d368681
alinneanais <alinneanais.rocha@gmail.com>
2017-06-12 14:34:09 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,70 @@
+
+<!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                  <p xmlns:dct="http://purl.org/dc/terms/">
+<a rel="license" href="http://creativecommons.org/publicdomain/mark/1.0/">
+</a>
+<br />
+Esta obra (<span property="dct:title">Juguetes y Memoria</span>, creada por <a href="https://github.com/alinneanais" rel="dct:creator"><span property="dct:title">Alinne Rocha</span></a>), está libre de restricciones conocidas de derecho de autor.
+</p>
+                </div>
+            </div>
+        </div>
+
+    <!-- Footer -->
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                        <ul class="list-inline text-center">
+                            <li>
+                                <a href="http://twitter.com/alinneanais">
+                                    <span class="fa-stack fa-lg">
+                                        <i class="fa fa-circle fa-stack-2x"></i>
+                                        <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="http://facebook.com/Aliianaiss">
+                                    <span class="fa-stack fa-lg">
+                                        <i class="fa fa-circle fa-stack-2x"></i>
+                                        <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/alinneanais">
+                                    <span class="fa-stack fa-lg">
+                                        <i class="fa fa-circle fa-stack-2x"></i>
+                                        <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                        <p class="copyright text-muted">Copyright &copy; Alinne Rocha - 2017</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    Fin de la conversación
+    Escribe un mensaje...
+
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/clean-blog.min.js"></script>
+
+</body>
---
+++ visualizaciones.php
@@ -0,0 +1,32 @@
+<?php include('header.php');?>
+<div class="container">
+  <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+ <!-- titulo -->
+      <h1 class="page-header">
+      Juguete Nacional
+        </h1>
+          <p></p>
+<!-- imagen -->
+<iframe width="560" height="315" src="https://www.youtube.com/embed/NBBcbgmgG7k" frameborder="0" allowfullscreen></iframe>
+<p><a href="https://www.youtube.com/embed/NBBcbgmgG7k">Spot sobre el juguete chileno</a> <a href="http://www.ccplm.cl/sitio/exposicion-juguete-nacional/">Captain Motion</a> on <a href="https://youtube.com">Youtube</a>.</p>    <!-- bajada -->
+
+        </div>
+  <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+ <!-- titulo -->
+
+      <h1 class="page-header">
+        De visita en el museo del juguete chileno
+        </h1>
+          <p>Visitamos la exposición "Historia del Juguete Chileno que se desarrolló en el espacio Museo Palacio de la Moneda, Santiago de Chile . Estas imagenes fueron obtenidas el Octubre 15, 2011. Esta exposición es un recorrido por la historia de las fabricas de juguetes que hubo en Chile, y fue posible apreciar como los niños de antes se entretenían con estas hoy piezas de museo. Interesante exposición.</p>
+<!-- imagen -->
+<iframe width="560" height="315" src="https://www.youtube.com/embed/vMjwpsvDgmA" frameborder="0" allowfullscreen></iframe>
+<p><a href="https://www.youtube.com/embed/vMjwpsvDgmA"> on <a href="https://youtube.com">Youtube</a>.</p>
+<!-- bajada -->
+
+        </div>
+        <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+       <!-- titulo -->
+
+
+
+<?php include('footer.php');?>
